7a7761eee88c3dd8d2814302c0a69630fa9db0b3
r.gusyakov <r.gusyakov@yadro.com>
2025-11-21 12:17:55 +0300

    chore: remove todo

--- app/src/main/java/i/love/building/webapps/its/actually/my/favourite/thing/in/the/world/especially/in/java/App.java
+++ app/src/main/java/i/love/building/webapps/its/actually/my/favourite/thing/in/the/world/especially/in/java/App.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// TODO: Specify OpenAPI documentation for endpoints with errors
-
 @RestController
 @SpringBootApplication
 public class App {
